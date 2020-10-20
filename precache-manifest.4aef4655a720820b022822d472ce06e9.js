f97b76e3f0b421f2012b67189520c0e2b87d4aa7
MilladMozhdehi <miladmozhdehipour73@gmail.com>
2020-10-20 18:10:28 +0330

    Updates

---
+++ precache-manifest.4aef4655a720820b022822d472ce06e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "306d99bffae8c1ff9742",
+    "url": "/TodoList/static/css/main.900ece9c.chunk.css"
+  },
+  {
+    "revision": "306d99bffae8c1ff9742",
+    "url": "/TodoList/static/js/main.306d99bf.chunk.js"
+  },
+  {
+    "revision": "3b7e3c6ab4b402ca65ac",
+    "url": "/TodoList/static/js/1.3b7e3c6a.chunk.js"
+  },
+  {
+    "revision": "e5fd00049e7ac7cce407",
+    "url": "/TodoList/static/js/runtime~main.e5fd0004.js"
+  },
+  {
+    "revision": "11568fd4c736b45efef06aef35b26612",
+    "url": "/TodoList/index.html"
+  }
+];
